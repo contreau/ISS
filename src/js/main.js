e529f7ae524a29fd397cade2ab23ff4bd75a5f03
c <cfk102@gmail.com>
2023-02-04 16:13:55 -0500

    adjusted folder structure

--- src/js/main.js
+++ src/js/main.js
@@ -17,7 +17,7 @@ let lastKnownLand = "Verifying..."; // Helps handle when water body returns unde
 
 // Fetches countries.json on page load
 const codes__json = new Promise((res, rej) => {
-  fetch("countries.json").then((data) => {
+  fetch("../countries.json").then((data) => {
     res(data.json());
   });
 });
@@ -56,7 +56,7 @@ const recenterMap = async (map) => {
 
 // ISS icon
 const ISSicon = L.icon({
-  iconUrl: "./img/iss.png",
+  iconUrl: "../img/iss.png",
   iconSize: [50, 50],
 });
 
